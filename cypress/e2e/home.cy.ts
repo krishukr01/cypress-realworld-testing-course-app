ba40f0745f9ef9e6e687c7387c6c9583a2e8e107
Krishan Kumar Safi <142893219+krishukr01@users.noreply.github.com>
2023-11-06 17:52:20 +0530

    Test cases modification

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,15 +3,24 @@ describe("home page", () => {
     cy.visit("http://localhost:3000")
   })
 
-  it("the h1 contains the correct word", () => {
-    cy.get("[data-test=hero-heading]")
-      .should("exist")
-      .contains("Testing Next.js Applications with Cypress")
+  context("hero section", () => {
+    it("the h1 contains the correct word", () => {
+      cy.get("[data-test=hero-heading]")
+        .should("exist")
+        .contains("Testing Next.js Applications with Cypress")
+    })
+
+    it("the features on the homepage are correct", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
   })
 
-  it.only("the features on the homepage are correct", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
+  context("course section", () => {
+    it.only("Courses: testing our first NextJs application", () => {
+      cy.getByData("course-0").find("a").eq(3).click()
+      cy.location("pathname").should("eq", "/testing-your-first-application")
+    })
   })
 })
--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -1,3 +1,24 @@
-describe('Newsletter Subscribe Form', () => {
-  
+describe("Newsletter Subscribe Form", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+  })
+  it("allow user to subscribes to the email list", () => {
+    cy.getByData("email-input").type("krishukrishan1211@gmail.com")
+    cy.getByData("submit-button").click()
+    cy.getByData("success-message")
+      .should("exist")
+      .contains("krishukrishan1211@gmail.com")
+  })
+  it("does not allow invalid email address", () => {
+    cy.getByData("email-input").type("krishan")
+    cy.getByData("submit-button").click()
+    cy.getByData("server-error-message").should("not.exist")
+    cy.getByData("success-message").should("not.exist")
+  })
+
+  it("does not allow already subscribed email address", () => {
+    cy.getByData("email-input").type("john@example.com")
+    cy.getByData("submit-button").click()
+    cy.getByData("server-error-message").should("exist")
+  })
 })
